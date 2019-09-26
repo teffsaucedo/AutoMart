1a09f744d8c1fdbdbee695a708cffd098cfd952d
Estefania Saucedo <estefaniasaucedo@hotmail.com>
2019-09-25 19:19:48 -0500

    Lista de varios autos

--- main_app/views.py
+++ main_app/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 def index (request):
-    nombre = "VW Jetta"
-    modelo = 2018
-    precio = 153000
-    color = "Gris"
+   
 
-    dict = { 'auto_nombre' : nombre,
-             'auto_modelo' : modelo,
-             'auto_precio' : precio,
-             'auto_color' : color,
 
-    }
+    return render(request, 'index.html', {'autos' : autos})
 
-    return render(request, 'index.html', dict)
+class Auto:
+    def __init__(self, nombre, precio, modelo, color):
+        self.nombre = nombre
+        self.precio = precio
+        self.modelo = modelo
+        self.color = color
+
+autos = [
+    Auto('VW Jetta', 145000, 2018, 'Gris'),
+    Auto ('Lexus', 256000, 2017, "Rojo"),
+    Auto ('Futura', 0, 1954, 'Agua'),
+    Auto ('Porsche', 25000, 2010, 'Azul'),
+]
